9cf36859471da7dd717e7c097f623879d34c3604
ezzat <ezatce117@outlook.com>
2022-08-22 21:52:30 +0300

    basics and router components

---
+++ src/components/Catalog.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Catalog extends Component {
+  render() {
+    return <div>Catalog component</div>;
+  }
+}
+
+export default Catalog;
---
+++ src/components/MovieDetail.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class MovieDetail extends Component {
+  render() {
+    return <div>MovieDetail component</div>;
+  }
+}
+
+export default MovieDetail;
